ec53f8651b5a28f56da658bef40c704bca62e282
Vidish <joglekar.vidish9@gmail.com>
2015-09-06 00:08:34 +1000

    Got the basic factory service working

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -1,21 +1,57 @@
 var flightsearch = angular.module('flightSearch', []);
+var flights;
 
-flightsearch.controller('flightList', function ($scope, $http) {
-    $http.get('flights-list.json').success(function (data) {
-        $scope.flights = data;
-        
-        $scope.addToList = function (data) {
+
+
+flightsearch.controller('flightList', ['$scope', '$http', 'flightSearchService', function ($scope, $http, flightSearchService) {
+        $scope.addToList = function(data) {
+            alert("I am in addToList");
+            $scope.flights = flightSearchService.getFlights();
+        }
+        //$scope.addToList = function (data) {
      
-            $scope.list = [];
-            angular.forEach($scope.flights, function(value,index){
-                if(value.source == $scope.from && value.destination == $scope.destination && value.departdate == $("#departuredate").datepicker({ dateFormat: 'dd,MM,yyyy' }).val() && value.returndate == $("#returndate").datepicker({ dateFormat: 'dd,MM,yyyy' }).val() )
-                {   
-                    $scope.list.push(value);
+            //$scope.list = [];
+            //angular.forEach($scope.flights, function(value,index){
+                //if(value.source == $scope.from && value.destination == $scope.destination && value.departdate == $("#departuredate").datepicker({ dateFormat: 'dd,MM,yyyy' }).val() && value.returndate == $("#returndate").datepicker({ dateFormat: 'dd,MM,yyyy' }).val() )
+               // {   
+                 //   $scope.list.push(value);
                     
-                }
+                //}
                 
-            });
-            return $scope.list;           
-        };
+            //});
+           // return $scope.list;           
+        //};
+    //});
+
+}]);
+
+
+
+flightsearch.factory('flightSearchService', function($http){
+    $http.get('flights-list.json').success(function (data) {
+        flights = data;
+        alert(data);
     });
+   
+   
+    return {
+        
+         
+        getFlights : function(){
+            
+    var list = [];
+          
+            alert(flights);
+    angular.forEach(flights, function(value, index){
+        if (value.source == "MEL")
+        {
+            list.push(value);
+            alert("I am just returning the flight list");
+        }
+    });
+            
+            
+    return list;
+}
+    };
 });
